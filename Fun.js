5147087497eeddf79d8d42a58c436ee85e8ac8a5
Vinaymeldrum <36042425+Vinaymeldrum@users.noreply.github.com>
2019-02-27 14:26:15 -0500

    Finished Fun

--- Fun.js
+++ Fun.js
@@ -1,18 +1,32 @@
-let counter = 0;
-let acc = 0;
+let freak = true;
 let speed = 1;
+let acc = 0;
+let counter = 0
+
 basic.forever(function () {
-
-    for (let j = 0; j < 5; j++) {
-        for (let i = 0; i < 5; i++) {
-            basic.pause(speed);
-            led.toggle(i, j);
-        }
-    }
-    speed++;
-
     input.onButtonPressed(Button.A, function () {
         speed = 1;
     })
 
+    input.onButtonPressed(Button.B, function () {
+        radio.sendNumber(0);
+    })
+    
+    radio.onReceivedNumber(function (receivedNumber: number) {
+        while (freak) {
+            for (let j = 0; j <= 4; j++) {
+                for (let i = 0; i <= 4; i++) {
+                    basic.pause(speed);
+                    led.toggle(i, j);
+                }
+                counter++;
+            }
+            if (speed > 50) {
+                speed = 0;
+                freak = false;
+            }
+            speed++;
+        }
+        led.enable(false);
+    })
 })
